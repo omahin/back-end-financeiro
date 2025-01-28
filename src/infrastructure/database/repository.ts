2a94bbc096e3cb8b7df23f0e2642141573e54e3d
Olga Mahin <olgamahin@hotmail.com>
2025-01-28 15:15:25 -0300

    :wastebasket: remove: Removendo arquivos não utilizados do projeto para manter a organização e atualização contínua

--- src/infrastructure/database/repository.ts
+++ src/infrastructure/database/repository.ts
@@ -3,26 +3,44 @@ import { Despesa } from '../../domain/despesa';
 import { DespesaModel } from './model';
 
 export class RepositoryData implements DespesaRepository {
-    async save (despesa: Despesa): Promise<void> {
-        const newDespesa = new DespesaModel(despesa)
+    async findById(despesaId: string): Promise<Despesa | null> {
+        const despesa = await DespesaModel.findById(despesaId);
+        if (!despesa) {
+            return null;
+        }
+        return {
+            id: despesa._id.toString(),
+            descricao: despesa.descricao,
+            categoria: despesa.categoria,
+            valor: despesa.valor,
+            tipo: despesa.tipo,
+            data: despesa.data,
+            userId: despesa.userId
+        } as Despesa;
+    }
+
+    async delete(despesaId: string): Promise<void> {
+        await DespesaModel.findByIdAndDelete(despesaId);
+    }
+
+    async save(despesa: Despesa): Promise<void> {
+        const newDespesa = new DespesaModel(despesa);
         await newDespesa.save();
     }
 
-    async findAll (): Promise<Array<Despesa>> {
-    const despesas = await DespesaModel.find();
-
-    const translatedDespesas = despesas.map(item => {
-        return {
-            id: item._id.toString(),
-            descricao: item.descricao,
-            categoria: item.categoria,
-            valor: item.valor,
-            tipo: item.tipo,
-            data: item.data,
-            userId: item.userId
-        }
-    }) as Array<Despesa>
-
-    return translatedDespesas;
+    async findAll(): Promise<Array<Despesa>> {
+        const despesas = await DespesaModel.find();
+        const translatedDespesas = despesas.map(item => {
+            return {
+                id: item._id.toString(),
+                descricao: item.descricao,
+                categoria: item.categoria,
+                valor: item.valor,
+                tipo: item.tipo,
+                data: item.data,
+                userId: item.userId
+            } as Despesa;
+        });
+        return translatedDespesas;
     }
 }
